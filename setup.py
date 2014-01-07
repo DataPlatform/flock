824dc9207f26586ece0e4f74ee7c78d7b5cf5438
Ramtin Kiany <ramtin.kiani@cfpb.gov>
2014-01-07 13:46:39 -0500

    setup.py installs requirements.txt now
    Still reading on entry point

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
+from pip.req import parse_requirements
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt')
+
+# reqs is a list of requirement
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='Flock',
@@ -11,6 +19,5 @@ setup(
     license='',
     description='Useful DB-related stuff.',
     long_description=open('README.md').read(),
-    install_requires=[
-    ],
+    install_requires=reqs,
 )
