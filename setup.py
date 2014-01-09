c3796d89047e1c7824e9fac1e6fb2d7170f7ea1f
foxs <stefan.fox@cfpb.gov>
2014-01-09 14:24:23 -0500

    temp test

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version='0.1.0',
     author='Stefan Fox',
     author_email='Stefan.Fox@cfpb.gov',
-    packages=['flock',],
+    packages=['flock','flock.tools'],
     scripts=[],
     url='https://github.com/DataPlatform/flock/wiki/',
     license='',
